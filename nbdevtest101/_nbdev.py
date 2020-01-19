a0a540008de1c651ddd9e336f20ef64919bfebf8
mathewhall100 <mathew.hall@dr.com>
2020-01-19 23:41:12 +0000

    bubble_scatter function added with example for docs

--- nbdevtest101/_nbdev.py
+++ nbdevtest101/_nbdev.py
@@ -3,7 +3,8 @@
 __all__ = ["index", "modules", "custom_doc_links", "git_url"]
 
 index = {"say_hello": "00_core.ipynb",
-         "HelloSayer": "00_core.ipynb"}
+         "HelloSayer": "00_core.ipynb",
+         "bubble_scatter": "00_core.ipynb"}
 
 modules = ["core.py"]
 
